0b63983637d948b76fb041d79cd94c55829b9953
zz <zz.at.field@gmail.com>
2016-07-21 11:55:32 +0800

    config

--- celerytask/celeryconfig.py
+++ celerytask/celeryconfig.py
@@ -1,3 +1,5 @@
 CELERY_IMPORTS = ['coordinator.tasks', 'coordinator.neuralnetwork']
+# BROKER_URL = 'amqp://test:test@192.168.2.104//'
 BROKER_URL = 'amqp://'
+# CELERY_RESULT_BACKEND = 'amqp://test:test@192.168.2.104//'
 CELERY_RESULT_BACKEND = 'amqp://'
